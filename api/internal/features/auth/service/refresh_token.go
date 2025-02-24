924d491d152a5506d9f9be97f4d1676fc527ff45
raghavyuva <vikramnbhat15@gmail.com>
2025-02-24 23:59:56 +0530

    adds logging for controllers and services of auth feature

--- api/internal/features/auth/service/refresh_token.go
+++ api/internal/features/auth/service/refresh_token.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/raghavyuva/nixopus-api/internal/features/auth/types"
+	"github.com/raghavyuva/nixopus-api/internal/features/logger"
 )
 
 // RefreshToken takes a refresh token as input and returns a new access token and refresh token. It
@@ -11,24 +12,29 @@ import (
 // revokes the old refresh token and creates a new one, and finally returns the new access token,
 // refresh token, the user's expiration time, and the user information.
 func (c *AuthService) RefreshToken(refreshToken types.RefreshTokenRequest) (types.AuthResponse, error) {
+	c.logger.Log(logger.Info, "refreshing token", refreshToken.RefreshToken)
 	token, err := c.storage.GetRefreshToken(refreshToken.RefreshToken)
 	if err != nil {
+		c.logger.Log(logger.Error, types.ErrInvalidRefreshToken.Error(), err.Error())
 		return types.AuthResponse{}, types.ErrInvalidRefreshToken
 	}
 
 	user, err := c.storage.FindUserByID(token.UserID.String())
 	if err != nil {
+		c.logger.Log(logger.Error, types.ErrUserNotFound.Error(), err.Error())
 		return types.AuthResponse{}, types.ErrUserNotFound
 	}
 
 	accessToken, err := createToken(user.Email, time.Minute*15)
 	if err != nil {
+		c.logger.Log(logger.Error, types.ErrFailedToCreateToken.Error(), err.Error())
 		return types.AuthResponse{}, types.ErrFailedToCreateToken
 	}
 
 	c.storage.RevokeRefreshToken(token.Token)
 	newRefreshToken, err := c.storage.CreateRefreshToken(user.ID)
 	if err != nil {
+		c.logger.Log(logger.Error, types.ErrFailedToCreateRefreshToken.Error(), err.Error())
 		return types.AuthResponse{}, types.ErrFailedToCreateRefreshToken
 	}
 

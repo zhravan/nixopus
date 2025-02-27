ccd42a63488ce28224dc8db37227d69d37b5e9da
raghavyuva <vikramnbhat15@gmail.com>
2025-02-27 19:59:25 +0530

    add swagger annotation for permission package

--- api/internal/features/permission/controller/create_permission.go
+++ api/internal/features/permission/controller/create_permission.go
@@ -9,8 +9,19 @@ import (
 	"github.com/raghavyuva/nixopus-api/internal/utils"
 )
 
+// CreatePermission godoc
+// @Summary Create a new permission
+// @Description Creates a new permission in the application.
+// @Tags permission
+// @Accept json
+// @Produce json
+// @Security BearerAuth
+// @Param create_permission body types.CreatePermissionRequest true "Create permission request"
+// @Success 201 {object} types.Response "Success response with permission"
+// @Failure 400 {object} types.Response "Bad request"
+// @Failure 500 {object} types.Response "Internal server error"
+// @Router /permissions [post]
 func (p *PermissionController) CreatePermission(w http.ResponseWriter, r *http.Request) {
-
 	var permission types.CreatePermissionRequest
 
 	if err := p.validator.ParseRequestBody(&permission, r.Body, &permission); err != nil {
--- api/internal/features/permission/controller/get_permission.go
+++ api/internal/features/permission/controller/get_permission.go
@@ -8,6 +8,16 @@ import (
 	"github.com/raghavyuva/nixopus-api/internal/utils"
 )
 
+// GetPermissions godoc
+// @Summary Get all permissions
+// @Description Retrieves all permissions.
+// @Tags permission
+// @Accept json
+// @Produce json
+// @Security BearerAuth
+// @Success 200 {array} types.Permission "Success response with permissions"
+// @Failure 500 {object} types.Response "Internal server error"
+// @Router /permissions [get]
 func (c *PermissionController) GetPermissions(w http.ResponseWriter, r *http.Request) {
 	permission, err := c.storage.GetPermissions()
 	if err != nil {
@@ -18,6 +28,18 @@ func (c *PermissionController) GetPermissions(w http.ResponseWriter, r *http.Req
 	utils.SendJSONResponse(w, "success", "Permissions fetched successfully", permission)
 }
 
+// GetPermission godoc
+// @Summary Get a permission
+// @Description Retrieves a permission by its ID.
+// @Tags permission
+// @Accept json
+// @Produce json
+// @Security BearerAuth
+// @Param id query string true "Permission ID"
+// @Success 200 {object} types.Permission "Success response with permission"
+// @Failure 400 {object} types.Response "Bad request"
+// @Failure 500 {object} types.Response "Internal server error"
+// @Router /permissions/{id} [get]
 func (c *PermissionController) GetPermission(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	request := types.GetPermissionRequest{

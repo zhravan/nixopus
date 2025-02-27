5fd2ceaa4a2928a0d5ff4ebab4bbc411c33225e2
raghavyuva <vikramnbhat15@gmail.com>
2025-02-27 20:02:57 +0530

    add swagger annotation for roles package

--- api/internal/features/role/controller/create_role.go
+++ api/internal/features/role/controller/create_role.go
@@ -9,7 +9,17 @@ import (
 	"github.com/raghavyuva/nixopus-api/internal/utils"
 )
 
-// CreateRole creates a new role in the database
+// CreateRole godoc
+// @Summary Create a new role
+// @Description Creates a new role in the application.
+// @Tags role
+// @Accept json
+// @Produce json
+// @Security BearerAuth
+// @Param create_role body types.CreateRoleRequest true "Create role request"
+// @Success 201 {object} types.Response "Success response with role"
+// @Failure 400 {object} types.Response "Bad request"
+// @Router /roles [post]
 func (c *RolesController) CreateRole(w http.ResponseWriter, r *http.Request) {
 	var role types.CreateRoleRequest
 

95fa5c05af563e3ebb18014271ed00f801028348
raghavyuva <vikramnbhat15@gmail.com>
2025-02-28 19:50:41 +0530

    create dashboard layout

--- view/app/page.tsx
+++ view/app/page.tsx
@@ -1,27 +1,23 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { logout } from "@/redux/features/users/authSlice";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useAppSelector } from "@/redux/hooks";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter()
   const authenticated = useAppSelector(state => state.auth.isAuthenticated)
-  const dispatch = useAppDispatch()
+
+  if (authenticated) {
+    router.push("/dashboard")
+    return < ></>
+  }
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="text-5xl">Hello, Welcome to Nixopus</div>
-      {authenticated ? (
-        <div className="mt-10 flex justify-center gap-20 text-2xl">
-          <Button onClick={() => dispatch(logout())}>Signout</Button>
-          <Button onClick={() => { }}>Home</Button>
-        </div>
-      ) : (
-        <div className="mt-10 flex justify-center gap-20 text-2xl">
-          <Button onClick={() => router.push("/login")}>Signin</Button>
-        </div>
-      )}
+      <div className="mt-10 flex justify-center gap-20 text-2xl">
+        <Button onClick={() => router.push("/login")}>Signin</Button>
+      </div>
     </div>
   );
 } 

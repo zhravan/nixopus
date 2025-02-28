429d028846223edd09378a8e3a13248826fbe8b8
raghavyuva <vikramnbhat15@gmail.com>
2025-02-28 18:34:58 +0530

    login hook to get the values of email and password during submission

--- view/app/login/page.tsx
+++ view/app/login/page.tsx
@@ -1,10 +1,20 @@
+"use client"
 import { LoginForm } from "@/components/login-form"
+import useLogin from "./hooks/use-login"
 
 export default function LoginPage() {
+  const { email, password, handleEmailChange, handlePasswordChange,handleLogin } = useLogin()
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+        <LoginForm
+          email={email}
+          password={password}
+          handleEmailChange={handleEmailChange}
+          handlePasswordChange={handlePasswordChange}
+          handleLogin={handleLogin}
+        />
       </div>
     </div>
   )

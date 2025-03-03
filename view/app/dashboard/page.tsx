5ed042837da54b047d23d4627349fc8a502f3a96
raghavyuva <vikramnbhat15@gmail.com>
2025-03-03 23:54:53 +0530

    github connector and github manifest authentication flow

--- view/app/dashboard/page.tsx
+++ view/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 'use client';
+import DashboardPageHeader from '@/components/dashboard-page-header';
 import { useAppSelector } from '@/redux/hooks';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import GitHubAppSetup from './components/github-connector/github-app-setup';
 
 function page() {
   const user = useAppSelector((state) => state.auth.user);
@@ -13,7 +15,12 @@ function page() {
     return null;
   }
 
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="container mx-auto py-6 space-y-8 max-w-4xl">
+      <DashboardPageHeader label="Dashboard" description={`Welcome, ${user.name}`} />
+      <GitHubAppSetup />
+    </div>
+  )
 }
 
 export default page;

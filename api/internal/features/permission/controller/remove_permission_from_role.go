92f7e7ee724745ddaf5015c714382ea46c787b99
raghavyuva <vikramnbhat15@gmail.com>
2025-03-04 19:17:27 +0530

    permission controllers common logic into init.go file of the respective controller file

--- api/internal/features/permission/controller/remove_permission_from_role.go
+++ api/internal/features/permission/controller/remove_permission_from_role.go
@@ -3,9 +3,7 @@ package controller
 import (
 	"net/http"
 
-	"github.com/raghavyuva/nixopus-api/internal/features/logger"
 	"github.com/raghavyuva/nixopus-api/internal/features/permission/types"
-	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
 	"github.com/raghavyuva/nixopus-api/internal/utils"
 )
 
@@ -23,15 +21,7 @@ import (
 func (c *PermissionController) RemovePermissionFromRole(w http.ResponseWriter, r *http.Request) {
 	var permission types.RemovePermissionFromRoleRequest
 
-	if err := c.validator.ParseRequestBody(permission, r.Body, &permission); err != nil {
-		c.logger.Log(logger.Error, err.Error(), "")
-		utils.SendErrorResponse(w, shared_types.ErrFailedToDecodeRequest.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if err := c.validator.ValidateRequest(permission); err != nil {
-		c.logger.Log(logger.Error, err.Error(), "")
-		utils.SendErrorResponse(w, err.Error(), http.StatusBadRequest)
+	if !c.parseAndValidate(w, r, &permission) {
 		return
 	}
 

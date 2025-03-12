583987457c05fc406c9595caacc1f777f6ef2554
raghavyuva <vikramnbhat15@gmail.com>
2025-03-12 16:34:27 +0530

    application related tables listen notify sql migration

---
+++ api/migrations/009_notify_row_changes_application_down.sql
@@ -0,0 +1,3 @@
+DROP TRIGGER IF EXISTS applications_notify ON applications;
+DROP TRIGGER IF EXISTS application_status_notify ON application_status;
+DROP TRIGGER IF EXISTS application_logs_notify ON application_logs;

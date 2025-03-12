d4c2be5b2bc40598834a8b6f813a9db32b841c10
raghavyuva <vikramnbhat15@gmail.com>
2025-03-12 20:35:47 +0530

    application deployment card design in progress

--- view/app/self-host/components/application-details/deploymentsList.tsx
+++ view/app/self-host/components/application-details/deploymentsList.tsx
@@ -1,7 +1,24 @@
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { ApplicationDeployment } from '@/redux/types/applications';
 import React from 'react';
 
-function DeploymentsList() {
-  return <div>DeploymentsList</div>;
+function DeploymentsList({ deployments }: { deployments?: ApplicationDeployment[] }) {
+  return (
+    <div className=''>
+      {
+        deployments?.map((deployment) => {
+          return (
+            <Card key={deployment.id}>
+              <CardHeader>
+                <CardTitle>#{deployment.id.slice(0, 6)}</CardTitle>
+                <CardDescription></CardDescription>
+              </CardHeader>
+            </Card>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default DeploymentsList;

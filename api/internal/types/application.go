02794d238838c4f0b036d7851ca250e761fdaa26
raghavyuva <vikramnbhat15@gmail.com>
2025-03-23 11:24:50 +0530

    add migration files for applications table to store dockerfile path

--- api/internal/types/application.go
+++ api/internal/types/application.go
@@ -9,27 +9,27 @@ import (
 
 type Application struct {
 	bun.BaseModel        `bun:"table:applications,alias:a" swaggerignore:"true"`
-	ID                   uuid.UUID   `json:"id" bun:"id,pk,type:uuid"`
-	Name                 string      `json:"name" bun:"name,notnull"`
-	Port                 int         `json:"port" bun:"port,notnull"`
-	Environment          Environment `json:"environment" bun:"environment,notnull"`
-	BuildVariables       string      `json:"build_variables" bun:"build_variables,notnull"`
-	EnvironmentVariables string      `json:"environment_variables" bun:"environment_variables,notnull"`
-	BuildPack            BuildPack   `json:"build_pack" bun:"build_pack,notnull"`
-	Repository           string      `json:"repository" bun:"repository,notnull"`
-	Branch               string      `json:"branch" bun:"branch,notnull"`
-	PreRunCommand        string      `json:"pre_run_command" bun:"pre_run_command,notnull"`
-	PostRunCommand       string      `json:"post_run_command" bun:"post_run_command,notnull"`
-	DomainID             uuid.UUID   `json:"domain_id" bun:"domain_id,notnull,type:uuid"`
-	UserID               uuid.UUID   `json:"user_id" bun:"user_id,notnull,type:uuid"`
-	CreatedAt            time.Time   `json:"created_at" bun:"created_at,notnull,default:current_timestamp"`
-	UpdatedAt            time.Time   `json:"updated_at" bun:"updated_at,notnull,default:current_timestamp"`
-
-	Domain      *Domain                  `json:"domain,omitempty" bun:"rel:belongs-to,join:domain_id=id"`
-	User        *User                    `json:"user,omitempty" bun:"rel:belongs-to,join:user_id=id"`
-	Status      *ApplicationStatus       `json:"status,omitempty" bun:"rel:has-one,join:id=application_id"`
-	Logs        []*ApplicationLogs       `json:"logs,omitempty" bun:"rel:has-many,join:id=application_id"`
-	Deployments []*ApplicationDeployment `json:"deployments,omitempty" bun:"rel:has-many,join:id=application_id"`
+	ID                   uuid.UUID                `json:"id" bun:"id,pk,type:uuid"`
+	Name                 string                   `json:"name" bun:"name,notnull"`
+	Port                 int                      `json:"port" bun:"port,notnull"`
+	Environment          Environment              `json:"environment" bun:"environment,notnull"`
+	BuildVariables       string                   `json:"build_variables" bun:"build_variables,notnull"`
+	EnvironmentVariables string                   `json:"environment_variables" bun:"environment_variables,notnull"`
+	BuildPack            BuildPack                `json:"build_pack" bun:"build_pack,notnull"`
+	Repository           string                   `json:"repository" bun:"repository,notnull"`
+	Branch               string                   `json:"branch" bun:"branch,notnull"`
+	PreRunCommand        string                   `json:"pre_run_command" bun:"pre_run_command,notnull"`
+	PostRunCommand       string                   `json:"post_run_command" bun:"post_run_command,notnull"`
+	DomainID             uuid.UUID                `json:"domain_id" bun:"domain_id,notnull,type:uuid"`
+	DockerfilePath       string                   `json:"dockerfile_path" bun:"dockerfile_path,notnull,default:Dockerfile"`
+	UserID               uuid.UUID                `json:"user_id" bun:"user_id,notnull,type:uuid"`
+	CreatedAt            time.Time                `json:"created_at" bun:"created_at,notnull,default:current_timestamp"`
+	UpdatedAt            time.Time                `json:"updated_at" bun:"updated_at,notnull,default:current_timestamp"`
+	Domain               *Domain                  `json:"domain,omitempty" bun:"rel:belongs-to,join:domain_id=id"`
+	User                 *User                    `json:"user,omitempty" bun:"rel:belongs-to,join:user_id=id"`
+	Status               *ApplicationStatus       `json:"status,omitempty" bun:"rel:has-one,join:id=application_id"`
+	Logs                 []*ApplicationLogs       `json:"logs,omitempty" bun:"rel:has-many,join:id=application_id"`
+	Deployments          []*ApplicationDeployment `json:"deployments,omitempty" bun:"rel:has-many,join:id=application_id"`
 }
 
 type ApplicationDeployment struct {

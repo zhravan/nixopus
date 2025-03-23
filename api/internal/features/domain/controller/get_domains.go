92603fc0e2aab24f364fb6a1e46918e3ffbfa528
raghavyuva <vikramnbhat15@gmail.com>
2025-03-23 14:36:40 +0530

    endpoint for generating random subdomain based on the domains available

--- api/internal/features/domain/controller/get_domains.go
+++ api/internal/features/domain/controller/get_domains.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/raghavyuva/nixopus-api/internal/features/logger"
 	"github.com/raghavyuva/nixopus-api/internal/utils"
@@ -24,4 +26,46 @@ func (c *DomainsController) GetDomains(w http.ResponseWriter, r *http.Request) {
 	}
 
 	utils.SendJSONResponse(w, "success", "Domains", domains)
+}
+
+func (c *DomainsController) GenerateRandomSubDomain(w http.ResponseWriter, r *http.Request) {
+	domains, err := c.service.GetDomains()
+	if err != nil {
+		c.logger.Log(logger.Error, err.Error(), "")
+		utils.SendErrorResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(domains) == 0 {
+		c.logger.Log(logger.Error, "no domains available", "")
+		utils.SendErrorResponse(w, "no domains available", http.StatusNotFound)
+		return
+	}
+
+	const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+	const prefixLength = 8
+	randomPrefix := make([]byte, prefixLength)
+	
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	
+	for i := range randomPrefix {
+		randomPrefix[i] = charset[random.Intn(len(charset))]
+	}
+	
+	randomDomain := domains[random.Intn(len(domains))]
+	
+	subdomain := string(randomPrefix) + "." + randomDomain.Name
+	
+	response := struct {
+		Subdomain string `json:"subdomain"`
+		Domain    string `json:"domain"`
+	}{
+		Subdomain: subdomain,
+		Domain:    randomDomain.Name,
+	}
+
+	c.logger.Log(logger.Info, "Generated random subdomain", subdomain)
+	
+	utils.SendJSONResponse(w, "success", "RandomSubdomain", response)
 }

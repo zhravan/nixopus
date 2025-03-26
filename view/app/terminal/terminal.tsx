71285680bfc43835fb8d5a815dffd50e55be6494
raghavyuva <vikramnbhat15@gmail.com>
2025-03-26 20:17:04 +0530

    show no users found if users length is less than or equal to 0

--- view/app/terminal/terminal.tsx
+++ view/app/terminal/terminal.tsx
@@ -38,8 +38,8 @@ export const IntegratedTerminal: React.FC<terminalProps> = ({
     }, [fitAddonRef]);
 
     return (
-        <div className="flex h-full flex-col border border-t-0">
-            <div className="flex h-5 items-center justify-between bg-secondary px-1 py-2 border-b-2 opacity-50">
+        <div className="flex h-full flex-col">
+            <div className="flex h-5 items-center justify-between bg-secondary px-1 py-2 opacity-50">
                 <span className="text-xs">
                     Terminal <span className="text-xs">⌘</span>J
                 </span>

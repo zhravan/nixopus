2da31256d736cd38e227accd94b1d41d3a9d3df9
raghavyuva <vikramnbhat15@gmail.com>
2025-03-31 15:21:47 +0530

    fix cors issues for wildcard does not work with authorization include

--- api/internal/middleware/cors.go
+++ api/internal/middleware/cors.go
@@ -1,14 +1,37 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // CorsMiddleware sets the necessary CORS headers for the response. If the request
 // method is OPTIONS, it will respond with a 200 status code and return. Otherwise,
 // it will call the next handler in the chain.
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS,PATCH")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := []string{
+			"https://app.nixopus.com",
+			"https://nixopus.com",
+		}
+		
+		originAllowed := false
+		for _, allowed := range allowedOrigins {
+			if origin == allowed {
+				originAllowed = true
+				break
+			}
+		}
+		
+		if originAllowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		} else if origin != "" {
+			if len(allowedOrigins) > 0 {
+				w.Header().Set("Access-Control-Allow-Origin", allowedOrigins[0])
+			}
+		}
+		
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token, Sec-WebSocket-Extensions, Sec-WebSocket-Key, Sec-WebSocket-Version")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")

333c1d1e7f3652de3589aaec0a7f5225dcb2c1fb
raghavyuva <vikramnbhat15@gmail.com>
2025-04-03 12:43:36 +0530

    refactor(login) :  move logics and effects into hooks

--- view/app/login/hooks/use-login.ts
+++ view/app/login/hooks/use-login.ts
@@ -1,6 +1,7 @@
+import { useAppSelector } from '@/redux/hooks';
 import { useLoginUserMutation } from '@/redux/services/users/authApi';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { toast } from 'sonner';
 
 function useLogin() {
@@ -8,6 +9,16 @@ function useLogin() {
   const [password, setPassword] = React.useState('');
   const [loginUser, { isLoading, error }] = useLoginUserMutation();
   const router = useRouter();
+
+  const authenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  const user = useAppSelector((state) => state.auth.user);
+
+  useEffect(() => {
+    if (authenticated && user) {
+      router.push('/dashboard');
+    }
+    return () => {};
+  }, [authenticated, user]);
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
--- view/app/login/page.tsx
+++ view/app/login/page.tsx
@@ -2,9 +2,6 @@
 import { LoginForm } from '@/components/features/login-form';
 import useLogin from './hooks/use-login';
 import { ErrorBoundary } from '@/components/ui/error-handler';
-import { useAppSelector } from '@/redux/hooks';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function LoginPage() {
   const {
@@ -16,16 +13,6 @@ export default function LoginPage() {
     isLoading,
     error
   } = useLogin();
-  const authenticated = useAppSelector((state) => state.auth.isAuthenticated);
-  const user = useAppSelector((state) => state.auth.user);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (authenticated && user) {
-      router.push('/dashboard');
-    }
-    return () => {};
-  }, [authenticated, user]);
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">

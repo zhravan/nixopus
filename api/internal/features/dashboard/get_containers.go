d354dfcaf3669a8bc1c3e5bb392920ba57ec1f48
raghavyuva <vikramnbhat15@gmail.com>
2025-04-04 23:06:11 +0530

    feat(dashboard) : send only top 4 containers based on their creation time

--- api/internal/features/dashboard/get_containers.go
+++ api/internal/features/dashboard/get_containers.go
@@ -1,6 +1,8 @@
 package dashboard
 
 import (
+	"sort"
+
 	"github.com/raghavyuva/nixopus-api/internal/features/logger"
 )
 
@@ -11,5 +13,14 @@ func (m *DashboardMonitor) GetContainers() {
 		m.BroadcastError(err.Error(), GetContainers)
 		return
 	}
+
+	sort.Slice(containers, func(i, j int) bool {
+		return containers[i].Created > containers[j].Created
+	})
+
+	if len(containers) > 4 {
+		containers = containers[:4]
+	}
+
 	m.Broadcast(string(GetContainers), containers)
 }

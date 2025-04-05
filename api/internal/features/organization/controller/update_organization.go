e890d61ad43213b04854aba73ec6616440ccbb0f
raghavyuva <vikramnbhat15@gmail.com>
2025-04-05 12:23:22 +0530

    update organization details such as name and description endpoint

--- api/internal/features/organization/controller/update_organization.go
+++ api/internal/features/organization/controller/update_organization.go
@@ -3,44 +3,43 @@ package controller
 import (
 	"net/http"
 
-	"github.com/raghavyuva/nixopus-api/internal/features/logger"
+	"github.com/go-fuego/fuego"
 	"github.com/raghavyuva/nixopus-api/internal/features/notification"
 	"github.com/raghavyuva/nixopus-api/internal/features/organization/types"
+	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
 	"github.com/raghavyuva/nixopus-api/internal/utils"
 )
 
-// UpdateOrganization godoc
-// @Summary Update an organization
-// @Description Updates an organization
-// @Tags organization
-// @Accept json
-// @Produce json
-// @Security BearerAuth
-// @Param update_organization body types.UpdateOrganizationRequest true "Update organization request"
-// @Success 200 {object} types.Response "Success response"
-// @Failure 400 {object} types.Response "Bad request"
-// @Failure 500 {object} types.Response "Internal server error"
-// @Router /organizations [put]
-func (c *OrganizationsController) UpdateOrganization(w http.ResponseWriter, r *http.Request) {
-	var organization types.UpdateOrganizationRequest
-
-	c.logger.Log(logger.Info, "updating organization", organization.ID)
-
-	loggedInUser := utils.GetUser(w, r)
-	if loggedInUser == nil {
-		return
+func (c *OrganizationsController) UpdateOrganization(f fuego.ContextWithBody[types.UpdateOrganizationRequest]) (*shared_types.Response, error) {
+	_, r := f.Response(), f.Request()
+	organization, err := f.Body()
+	if err != nil {
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusBadRequest,
+		}
 	}
 
-	if !c.parseAndValidate(w, r, &organization) {
-		return
+	loggedInUser := utils.GetUser(f.Response(), r)
+	if loggedInUser == nil {
+		return nil, fuego.HTTPError{
+			Err:    nil,
+			Status: http.StatusUnauthorized,
+		}
 	}
 
 	if err := c.service.UpdateOrganization(&organization); err != nil {
-		utils.SendErrorResponse(w, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusInternalServerError,
+		}
 	}
 
 	c.Notify(notification.NotificationPayloadTypeUpdateOrganization, loggedInUser, r)
 
-	utils.SendJSONResponse(w, "success", "Organization updated successfully", nil)
+	return &shared_types.Response{
+		Status:  "success",
+		Message: "Organization updated successfully",
+		Data:    nil,
+	}, nil
 }

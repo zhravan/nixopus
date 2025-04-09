75e4ed32e4ee18a4ffd201cd9e9a1ac9dc0bb229
raghavyuva <vikramnbhat15@gmail.com>
2025-04-09 18:09:50 +0530

    fix(application-fetch) : fetch application was giving empty since active organization is not updated to send x-org-id header and it sends wrong orgnanization id to the backend thus by creating a bug and fetching wrong orgs apps

--- api/internal/features/deploy/service/get_applications.go
+++ api/internal/features/deploy/service/get_applications.go
@@ -16,7 +16,7 @@ func (s *DeployService) GetApplications(page string, pageSize string, organizati
 	if err != nil {
 		return nil, 0, err
 	}
-	applications, totalCount, err := s.storage.GetApplications(pageNum, pageSizeNum,organizationID)
+	applications, totalCount, err := s.storage.GetApplications(pageNum, pageSizeNum, organizationID)
 	if err != nil {
 		return nil, 0, err
 	}

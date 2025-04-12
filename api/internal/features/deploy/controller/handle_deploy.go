881a76ecad0ac678ac9f35cb6589101343907a48
raghavyuva <vikramnbhat15@gmail.com>
2025-04-12 13:45:07 +0530

    refactor controllers of the deploy service in seperate files for better readability

---
+++ api/internal/features/deploy/controller/handle_deploy.go
@@ -0,0 +1,71 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/go-fuego/fuego"
+	"github.com/google/uuid"
+	"github.com/raghavyuva/nixopus-api/internal/features/deploy/types"
+	"github.com/raghavyuva/nixopus-api/internal/features/logger"
+	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
+	"github.com/raghavyuva/nixopus-api/internal/utils"
+)
+
+func (c *DeployController) HandleDeploy(f fuego.ContextWithBody[types.CreateDeploymentRequest]) (*shared_types.Response, error) {
+	c.logger.Log(logger.Info, "starting deployment process", "")
+
+	data, err := f.Body()
+	if err != nil {
+		c.logger.Log(logger.Error, "failed to read request body", err.Error())
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusBadRequest,
+		}
+	}
+
+	c.logger.Log(logger.Info, "request body parsed successfully", "name: "+data.Name)
+
+	if err := c.validator.ValidateRequest(&data); err != nil {
+		c.logger.Log(logger.Error, "request validation failed", "name: "+data.Name+", error: "+err.Error())
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusBadRequest,
+		}
+	}
+
+	user := utils.GetUser(f.Response(), f.Request())
+	if user == nil {
+		c.logger.Log(logger.Error, "user authentication failed", "name: "+data.Name)
+		return nil, fuego.HTTPError{
+			Err:    nil,
+			Status: http.StatusUnauthorized,
+		}
+	}
+
+	organizationID := utils.GetOrganizationID(f.Request())
+	if organizationID == uuid.Nil {
+		c.logger.Log(logger.Error, "organization not found", "name: "+data.Name)
+		return nil, fuego.HTTPError{
+			Err:    nil,
+			Status: http.StatusUnauthorized,
+		}
+	}
+
+	c.logger.Log(logger.Info, "attempting to create deployment", "name: "+data.Name+", user_id: "+user.ID.String())
+
+	application, err := c.service.CreateDeployment(&data, user.ID, organizationID)
+	if err != nil {
+		c.logger.Log(logger.Error, "failed to create deployment", "name: "+data.Name+", error: "+err.Error())
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusInternalServerError,
+		}
+	}
+
+	c.logger.Log(logger.Info, "deployment created successfully", "name: "+data.Name)
+	return &shared_types.Response{
+		Status:  "success",
+		Message: "Deployment created successfully",
+		Data:    application,
+	}, nil
+}
---
+++ api/internal/features/deploy/controller/redeploy_application.go
@@ -0,0 +1,79 @@
+package controller
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/go-fuego/fuego"
+	"github.com/google/uuid"
+	"github.com/raghavyuva/nixopus-api/internal/features/deploy/types"
+	"github.com/raghavyuva/nixopus-api/internal/features/logger"
+	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
+	"github.com/raghavyuva/nixopus-api/internal/utils"
+)
+
+func (c *DeployController) ReDeployApplication(f fuego.ContextWithBody[types.ReDeployApplicationRequest]) (*shared_types.Response, error) {
+	c.logger.Log(logger.Info, "starting application redeployment process", "")
+
+	data, err := f.Body()
+	if err != nil {
+		if err == io.EOF {
+			c.logger.Log(logger.Error, "empty request body received", "id is required for redeployment")
+			return nil, fuego.HTTPError{
+				Err:    types.ErrMissingID,
+				Status: http.StatusBadRequest,
+			}
+		}
+		c.logger.Log(logger.Error, "failed to read request body", err.Error())
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusBadRequest,
+		}
+	}
+
+	c.logger.Log(logger.Info, "request body parsed successfully", "id: "+data.ID.String())
+
+	if err := c.validator.ValidateRequest(&data); err != nil {
+		c.logger.Log(logger.Error, "request validation failed", "id: "+data.ID.String()+", error: "+err.Error())
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusBadRequest,
+		}
+	}
+
+	user := utils.GetUser(f.Response(), f.Request())
+	if user == nil {
+		c.logger.Log(logger.Error, "user authentication failed", "id: "+data.ID.String())
+		return nil, fuego.HTTPError{
+			Err:    nil,
+			Status: http.StatusUnauthorized,
+		}
+	}
+
+	organizationID := utils.GetOrganizationID(f.Request())
+	if organizationID == uuid.Nil {
+		c.logger.Log(logger.Error, "organization not found", "id: "+data.ID.String())
+		return nil, fuego.HTTPError{
+			Err:    nil,
+			Status: http.StatusUnauthorized,
+		}
+	}
+
+	c.logger.Log(logger.Info, "attempting to redeploy application", "id: "+data.ID.String()+", user_id: "+user.ID.String())
+
+	application, err := c.service.ReDeployApplication(&data, user.ID, organizationID)
+	if err != nil {
+		c.logger.Log(logger.Error, "failed to redeploy application", "id: "+data.ID.String()+", error: "+err.Error())
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusInternalServerError,
+		}
+	}
+
+	c.logger.Log(logger.Info, "application redeployed successfully", "id: "+data.ID.String())
+	return &shared_types.Response{
+		Status:  "success",
+		Message: "Application redeployed successfully",
+		Data:    application,
+	}, nil
+}

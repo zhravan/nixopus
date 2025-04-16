4561a41df74f8d919edccabfa211bebb5a5da721
raghavyuva <vikramnbhat15@gmail.com>
2025-04-17 01:13:20 +0530

    chore(view-dockerfile): update to port config mismatch in view build

--- view/next.config.ts
+++ view/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  env: {
+    PORT: process.env.NEXT_PUBLIC_PORT || '7443'
+  }
 };
 
 export default nextConfig;

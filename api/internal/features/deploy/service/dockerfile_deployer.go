8dc9acf8fe082d470341532ebff41ab704a63ae7
raghavyuva <vikramnbhat15@gmail.com>
2025-05-04 21:51:30 +0530

    fix(self-host): port mapping to match with what caddy listens as a proxy service

--- api/internal/features/deploy/service/dockerfile_deployer.go
+++ api/internal/features/deploy/service/dockerfile_deployer.go
@@ -2,8 +2,6 @@ package service
 
 import (
 	"fmt"
-	"strconv"
-
 	"github.com/raghavyuva/nixopus-api/internal/features/deploy/proxy"
 	"github.com/raghavyuva/nixopus-api/internal/features/deploy/types"
 	"github.com/raghavyuva/nixopus-api/internal/features/logger"
@@ -12,11 +10,12 @@ import (
 func (s *DeployService) handleDockerfileDeployment(d DeployerConfig) error {
 	s.addLog(d.application.ID, types.LogUsingDockerfileStrategy, d.deployment_config.ID)
 	s.addLog(d.application.ID, fmt.Sprintf(types.LogBuildContextPath, d.contextPath), d.deployment_config.ID)
-	if err := s.buildAndRunDockerImage(d); err != nil {
+	availablePort, err := s.buildAndRunDockerImage(d)
+	if err != nil {
 		return err
 	}
 
-	caddyProxy := proxy.NewCaddy(&s.logger, d.contextPath, d.application.Domain, strconv.Itoa(d.application.Port), proxy.ReverseProxy)
+	caddyProxy := proxy.NewCaddy(&s.logger, d.contextPath, d.application.Domain, availablePort, proxy.ReverseProxy)
 	if err := caddyProxy.Serve(); err != nil {
 		s.addLog(d.application.ID, fmt.Sprintf("Failed to start Caddy proxy: %v", err), d.deployment_config.ID)
 		return err
@@ -26,23 +25,23 @@ func (s *DeployService) handleDockerfileDeployment(d DeployerConfig) error {
 	return nil
 }
 
-func (s *DeployService) buildAndRunDockerImage(d DeployerConfig) error {
+func (s *DeployService) buildAndRunDockerImage(d DeployerConfig) (string, error) {
 	_, err := s.buildImageFromDockerfile(d)
 	if err != nil {
 		s.addLog(d.application.ID, fmt.Sprintf(types.LogFailedToBuildDockerImage, err.Error()), d.deployment_config.ID)
-		return fmt.Errorf("%w: %v", types.ErrBuildDockerImage, err)
+		return "", fmt.Errorf("%w: %v", types.ErrBuildDockerImage, err)
 	}
 
 	s.logger.Log(logger.Info, types.LogDockerImageBuiltSuccessfully, d.application.Name)
 	s.addLog(d.application.ID, types.LogDockerImageBuiltSuccessfully, d.deployment_config.ID)
-	containerID, err := s.AtomicUpdateContainer(d)
+	containerID, availablePort, err := s.AtomicUpdateContainer(d)
 	if err != nil {
 		s.addLog(d.application.ID, fmt.Sprintf(types.LogFailedToRunDockerImage, err.Error()), d.deployment_config.ID)
-		return fmt.Errorf("%w: %v", types.ErrRunDockerImage, err)
+		return "", fmt.Errorf("%w: %v", types.ErrRunDockerImage, err)
 	}
 
 	s.addLog(d.application.ID, fmt.Sprintf(types.LogContainerRunning, containerID), d.deployment_config.ID)
 	s.addLog(d.application.ID, fmt.Sprintf(types.LogApplicationExposed, d.application.Port), d.deployment_config.ID)
 
-	return nil
+	return availablePort, nil
 }

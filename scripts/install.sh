da8ae9c1e9088cc74814f3c48066919a89d49bc8
raghavyuva <vikramnbhat15@gmail.com>
2025-05-29 18:47:39 +0530

    refactor: Add comments and use functions for scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+# check if the script is running as root
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root (sudo)"
     exit 1
 fi
 
-for cmd in python3 pip3 git; do
-    if ! command -v $cmd &> /dev/null; then
-        echo "$cmd is not installed. Please install $cmd before running this script."
+# check if the required commands are installed
+function check_command() {
+    if ! command -v $1 &> /dev/null; then
+        echo "$1 is not installed. Please install $1 before running this script."
         exit 1
     fi
-done
+}
 
+# check if the required python version is installed
+function check_python_version() {
+    if ! python3 --version | grep -q "Python 3.10"; then
+        echo "Python 3.10 is not installed. Please install Python 3.10 before running this script."
+        # we will have to install python 3.10 here based on the OS
+        exit 1
+    fi
+}
+
+# check if the required commands are installed
+check_command "python3"
+check_command "pip3"
+check_command "git"
+
+check_python_version
+
+# default to production environment if no environment is specified through the command line arguments
 ENV="production"
+
+# parse the command line arguments
 for arg in "$@"; do
     if [[ $arg == "--env"* ]]; then
         ENV=$(echo $arg | cut -d'=' -f2)
@@ -20,6 +41,7 @@ for arg in "$@"; do
     fi
 done
 
+# set the source and destination directories based on the environment
 if [ "$ENV" == "staging" ]; then
     NIXOPUS_DIR="/etc/nixopus-staging"
     SOURCE_DIR="$NIXOPUS_DIR/source"
@@ -30,9 +52,11 @@ else
     BRANCH="master"
 fi
 
+# create the directories if they don't exist
 mkdir -p $NIXOPUS_DIR
 mkdir -p $SOURCE_DIR
 
+# clone the repository if it doesn't exist
 if [ -d "$SOURCE_DIR/.git" ]; then
     cd $SOURCE_DIR
     git fetch --all
@@ -46,6 +70,7 @@ else
     git checkout $BRANCH
 fi
 
+# set up the caddy configuration
 echo "Setting up Caddy configuration..."
 rm -rf $NIXOPUS_DIR/caddy > /dev/null 2>&1
 mkdir -p $NIXOPUS_DIR/caddy > /dev/null 2>&1
@@ -67,6 +92,8 @@ pip install --upgrade pip > /dev/null 2>&1
 pip install -r requirements.txt > /dev/null 2>&1
 
 echo "Starting Nixopus Installation..."
+# run the installer
 PYTHONPATH=$SOURCE_DIR/installer python3 install.py "$@"
 
+# deactivate the virtual environment
 deactivate > /dev/null 2>&1

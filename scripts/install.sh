12ac7f4b33fe3a6b9675fb2a4f17dc2a7a39cde4
raghavyuva <vikramnbhat15@gmail.com>
2025-06-13 19:07:45 +0530

    chore: change staging installation to happen through the current branch

--- scripts/install.sh
+++ scripts/install.sh
@@ -52,7 +52,7 @@ function setup_config_based_on_environment() {
     if [ "$env" == "staging" ]; then
         NIXOPUS_DIR="/etc/nixopus-staging"
         SOURCE_DIR="$NIXOPUS_DIR/source"
-        BRANCH="feat/develop"
+        BRANCH="feat/fix_domain_deployment"
     else
         NIXOPUS_DIR="/etc/nixopus"
         SOURCE_DIR="$NIXOPUS_DIR/source"

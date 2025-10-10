15e4005cd9e060856e35ef3bb947a6f3e8b44f64
zhravan <mrshravankumarb@gmail.com>
2025-10-11 00:25:24 +0530

    hotfix(view): AppInfo Env naming for client side config

--- view/app/config/appInfo.ts
+++ view/app/config/appInfo.ts
@@ -1,8 +1,8 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: 'Nixopus',
-  apiDomain: process.env.API_URL?.replace('/api', '') || 'http://localhost:8080',
-  websiteDomain: process.env.WEBSITE_DOMAIN || 'http://localhost:3000',
+  apiDomain: process.env.NEXT_PUBLIC_API_URL?.replace('/api', ''),
+  websiteDomain: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || 'http://localhost:3000',
   apiBasePath: '/auth',
   websiteBasePath: '/auth'
 };

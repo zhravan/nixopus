360d17ea0838b5421182c9f58a4ea0b8bc6780de
shravan || श्रvan <mrshravankumarb@gmail.com>
2025-10-11 01:04:56 +0530

    hotfix(ci): linter build for view (#480)

--- view/app/config/appInfo.ts
+++ view/app/config/appInfo.ts
@@ -1,7 +1,7 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: 'Nixopus',
-  apiDomain: process.env.NEXT_PUBLIC_API_URL?.replace('/api', ''),
+  apiDomain: process.env.NEXT_PUBLIC_API_URL?.replace('/api', '') || 'http://localhost:8080',
   websiteDomain: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || 'http://localhost:3000',
   apiBasePath: '/auth',
   websiteBasePath: '/auth'

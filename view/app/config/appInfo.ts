7d6ecb0194eba034bfd2cdb8216a461777a26503
zhravan <mrshravankumarb@gmail.com>
2025-10-12 11:00:33 +0530

    fix: access Website domain via config call

--- view/app/config/appInfo.ts
+++ view/app/config/appInfo.ts
@@ -1,13 +1,9 @@
-import { getBaseUrl } from '@/redux/conf';
+import { getBaseUrl, getWebsiteDomain } from '@/redux/conf';
 
 export const getAppInfo = async () => {
   const baseUrl = await getBaseUrl();
   const apiDomain = baseUrl.replace('://', 'TEMP').replace('/api', '').replace('TEMP', '://');
-
-  const websiteDomain = 
-    process.env.VIEW_DOMAIN || 
-    process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || 
-    'http://localhost:3000';
+  const websiteDomain = await getWebsiteDomain();
 
   return {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo

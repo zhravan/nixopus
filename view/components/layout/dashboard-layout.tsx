c5697a344c8e7aa8f635eb741a00f23ff6857dc3
Suma Bhat <136444974+Sumabhat11@users.noreply.github.com>
2025-12-09 22:32:46 +0530

    feat(ui): pointer cursor when hover over breadcrumbs (#608)

--- view/components/layout/dashboard-layout.tsx
+++ view/components/layout/dashboard-layout.tsx
@@ -83,7 +83,10 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                     {breadcrumbs.map((breadcrumb, idx) => (
                       <React.Fragment key={idx}>
                         <BreadcrumbItem className="hidden md:block">
-                          <BreadcrumbLink onClick={() => router.push(breadcrumb.href)}>
+                          <BreadcrumbLink
+                            onClick={() => router.push(breadcrumb.href)}
+                            className="cursor-pointer"
+                          >
                             {breadcrumb.label}
                           </BreadcrumbLink>
                         </BreadcrumbItem>

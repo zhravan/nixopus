1a30c310b69a799201fc633612c496c01fd24a79
raghavyuva <vikramnbhat15@gmail.com>
2025-12-27 15:20:27 +0530

    docs: fix generator command registration in vitepress

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,9 +7,13 @@ import './style.css'
 
 import spec from '../../src/openapi.json' assert { type: 'json' }
 
+import InstallGenerator from '../components/InstallGenerator.vue'
+
 export default {
     extends: DefaultTheme,
     async enhanceApp(ctx) {
+        ctx.app.component('InstallGenerator', InstallGenerator)
+        
         const openapi = useOpenapi({ spec,
             config: {
                 server: {

028b9333e1aac4702a251353ebef3aa64e8f2b9d
raghavyuva <vikramnbhat15@gmail.com>
2026-02-28 15:29:20 +0530

    feat(view): add workflow list and management hooks

---
+++ view/packages/hooks/workflows/index.ts
@@ -0,0 +1,23 @@
+export {
+  useWorkflows,
+  useWorkflowDetail,
+  useToolRegistry,
+  useNodeDescription,
+  useNodePalette,
+  type WorkflowDetail,
+  type PlanningMessageDetail,
+  type DynamicWorkflowDetail
+} from './workflows';
+
+export {
+  usePanelToggle,
+  useScrollToBottom,
+  useStreamingPlaceholder,
+  useWorkflowPlanner,
+  useChatTimeline,
+  useWorkflowRunner,
+  useWorkflowEditor,
+  type PlannerMessage,
+  type WorkflowGraph,
+  type TimelineItem
+} from './workflow-editor';

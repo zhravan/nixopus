a964d31b78a5a713058b49693a92e8fe598fa963
raghavyuva <vikramnbhat15@gmail.com>
2026-03-04 11:09:45 +0530

    fix: add images unoptimized for view

--- view/next.config.ts
+++ view/next.config.ts
@@ -4,10 +4,8 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   basePath: process.env.BASE_PATH || '',
   assetPrefix: process.env.ASSET_PREFIX || undefined,
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['dashboard.nixopus.com']
-    }
+  images: {
+    unoptimized: true
   }
 };
 

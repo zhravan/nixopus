79f009f585a7441c1a71dee671087e44acc5e1d2
Raghavendra Bhat <53376933+raghavyuva@users.noreply.github.com>
2026-03-31 13:19:05 +0530

    chore: migrate docs to mintlify and move out to nixopus/docs (#1166)

--- docs/.vitepress/components/install-generator/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TooltipIcon } from './TooltipIcon.vue'
-export { default as ValidationMessage } from './ValidationMessage.vue'
-export { default as ValidationMessages } from './ValidationMessages.vue'
